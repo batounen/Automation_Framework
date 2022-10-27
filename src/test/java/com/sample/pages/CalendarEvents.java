affe9243e8af978f351c7f61668d79ea7e6dc3de
Bato Unen <batounen@gmail.com>
2022-10-27 17:51:16 -0600

    CalendarEvents created

---
+++ src/test/java/com/sample/pages/CalendarEvents.java
@@ -0,0 +1,23 @@
+package com.sample.pages;
+
+import com.sample.utils.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CalendarEvents {
+
+    public CalendarEvents() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(css = ".btn-primary")
+    private WebElement createCalendarEventButton;
+
+    public void clickCreateCalendarEventButton() {
+        createCalendarEventButton.click();
+        Driver.waitForTitle(Driver.getProperty("createCalendarEvent"), 10);
+        Driver.sleep(2);
+    }
+
+}
