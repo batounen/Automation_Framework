49e449e57e6d13c8f153cbb8fba67af496ba7646
Bato Unen <batounen@gmail.com>
2022-10-27 18:23:08 -0600

    US71 created

---
+++ src/test/java/com/sample/tests/us71/US71.java
@@ -0,0 +1,33 @@
+package com.sample.tests.us71;
+
+import com.sample.pages.Home;
+import com.sample.pages.Login;
+import com.sample.utils.Driver;
+import com.sample.utils.TestBase;
+import org.testng.annotations.*;
+
+public class US71 extends TestBase {
+
+    /*
+    As a user, I should be able to access the Oroinc Documentation page.
+    AC #1: users access the Oronic Documentation page by clicking the question icon. The page URL: https://doc.oroinc.com/
+     */
+
+    @DataProvider(name = "dp")
+    public Object[][] dataProvider() {
+        return Driver.readXLSX();
+    }
+
+    @Test(dataProvider = "dp")
+    public void oroinc_doc_test(String username, String password) {
+        login = new Login();
+        login.login_positive(username, password);
+        Driver.sleep(3);
+
+        home = new Home();
+        home.getHelp_button_test();
+
+        home.logout();
+    }
+
+}
