4b65c7107d2c83774fc2261d696c0fd5c160af22
Bato Unen <batounen@gmail.com>
2022-10-27 19:10:22 -0600

    ActivitiesModule updated

--- src/test/java/com/sample/pages/ActivitiesModule.java
+++ src/test/java/com/sample/pages/ActivitiesModule.java
@@ -1,39 +1,36 @@
 package com.sample.pages;
 
 import com.sample.utils.Driver;
+import com.sample.utils.TestBase;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class ActivitiesModule {
+public class ActivitiesModule extends TestBase {
 
     public ActivitiesModule() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(css = "li[data-route='oro_call_index'] a")
-    private WebElement calls;
+    private WebElement call;
 
     @FindBy(css = "li[data-route='oro_calendar_event_index'] a")
-    private WebElement calendarEvents;
-
-    public WebElement getCalendarEvents() {
-        return calendarEvents;
-    }
+    private WebElement calendarEvent;
 
     public void access_activities_module(String moduleName) {
         Home home = new Home();
         home.hover_to_module(Driver.getProperty("activities"));
         switch (moduleName) {
             case "Calls":
-                Driver.captureHighlighted(calls);
-                calls.click();
-                Driver.sleep(2);
+                Driver.captureHighlighted(call);
+                call.click();
+                Driver.waitUntilClickable(calls.getLogCallButton(), 10);
                 break;
             case "Calendar Events":
-                Driver.captureHighlighted(calendarEvents);
-                calendarEvents.click();
-                Driver.sleep(2);
+                Driver.captureHighlighted(calendarEvent);
+                calendarEvent.click();
+                Driver.waitUntilClickable(calendarEvents.getCreateCalendarEventButton(), 10);
                 break;
         }
     }
