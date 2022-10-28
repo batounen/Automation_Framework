9319c674d693ac9c1e41a40a7f4f01360259d831
Bato Unen <batounen@gmail.com>
2022-10-27 18:01:02 -0600

    VehicleContracts updated

--- src/test/java/com/sample/pages/VehicleContracts.java
+++ src/test/java/com/sample/pages/VehicleContracts.java
@@ -4,13 +4,7 @@ import com.sample.utils.Driver;
 import org.openqa.selenium.support.PageFactory;
 
 public class VehicleContracts {
-
     public VehicleContracts() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
-
-
-
-
-
 }
